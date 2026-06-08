53efc2bb34097a3ea5ac11b6824246dad90121bf
Umut Camliyurt <83503290+umutcamliyurt@users.noreply.github.com>
2026-06-08 19:17:50 +0000

    Update versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.umut.irischat"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 104
         versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
