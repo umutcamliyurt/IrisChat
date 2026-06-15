29726ea3333a3118f79e8fc0f5a5096ca62f0a97
Umut Camliyurt <83503290+umutcamliyurt@users.noreply.github.com>
2026-06-15 00:22:50 +0000

    Improve message bubbles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.umut.irischat"
         minSdk = 24
         targetSdk = 34
-        versionCode = 131
-        versionName = "1.3.1"
+        versionCode = 132
+        versionName = "1.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
 
